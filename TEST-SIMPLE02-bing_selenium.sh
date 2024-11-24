ef998396836e8905327e35f1ef47f6958b2d48c7
David Bainbridge <davidb@waikato.ac.nz>
2024-11-24 21:21:23 +1300

    Further refactoring of code; more consitent use of config and args, now stored as 'globals'; bing search has accidentally written for google page scraping; base64 decode for bing debugged as made its own routine

---
+++ TEST-SIMPLE02-bing_selenium.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+rm -rf querydownload.db downloads \
+    && ./lrl-crawler.py --lang MAORI \
+			--all \
+			--query_count 1 \
+			--search_engine bing_selenium \
+			--download_with_selenium \
+			--num_threads 4 --num_pages 1 \
+			$@
---
+++ globals.py
@@ -0,0 +1,9 @@
+
+# Stores the result of reading in config.jon
+config = None
+
+# Stores the result of processing the command-line arguments
+args   = None
+
+# Provides a handier version of the command-line argument -v/--verbose to access
+verbose = 1
