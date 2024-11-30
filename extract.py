68eb742861f3de6ae22e331075f2ea3ead95c63b
David Bainbridge <davidb@waikato.ac.nz>
2024-12-01 09:57:37 +1300

    Some function refactoring on breaking up text into tokens; Also some initial work on identifying paras from downloaded web pages to use are regenerated frequenc dict

--- extract.py
+++ extract.py
@@ -3,10 +3,13 @@ import os
 import re
 import sys
 
-from collections import Counter
+#from collections import Counter
+import collections
 from pdfminer.high_level import extract_text as pdfminer_extract_text
 
 import globals
+import nlp
+import sql
 import utils
 
 
@@ -28,19 +31,26 @@ def extract_pdf(path):
         exit(1)
 
 
-def preprocess_text(text):
+def preprocess_text_into_words(text):
     """Tokenize the content"""
-    return re.findall(r'\b\w+\b', text.lower())
+    #return re.findall(r'\b\w+\b', text.lower())
+    return re.findall(r'\b\w+\b', text)
 
 
+def filter_words(words, min_char_len=3):
+    """Remove any words that are combined with digits, e.g. xyz12"""
+
+    filtered_lc_words = [word.lower() for word in words if len(word) >= min_char_len and not any(char.isdigit() for char in word)]
+
+    return filtered_lc_words
+    
 def get_common_words(tokens, min_length=3):
     """Filter out words based on length and get all words"""
 
     # Also removes any digits
-    filtered_tokens = [word for word in tokens if len(
-        word) >= min_length and not any(char.isdigit() for char in word)]
+    # filtered_tokens = [word for word in tokens if len(word) >= min_length and not any(char.isdigit() for char in word)]
 
-    word_freq = Counter(filtered_tokens)
+    word_freq = collections.Counter(tokens)
     most_common = word_freq.most_common() # returns all frequency counts if no int-param passed in
     
     return most_common
@@ -63,11 +73,11 @@ def extract_udhr(lang_initialcap,force=False):
 
         # Check if file exists and act according to `force` parameter
         filename = f"dicts/common_words_{config_language.lower()}.json"
-        if os.path.exists(filename):
-            print(f"  Skipping generating output file '{filename}' as this already exists.")
-            print(f"  To regenerate/update this file, remove this file before running the script, or use -force to overwrite.")
-            continue
-        
+        #if os.path.exists(filename):
+        #    print(f"  Skipping generating output file '{filename}' as this already exists.")
+        #    print(f"  To regenerate/update this file, remove this file before running the script, or use -force to overwrite.")
+        #    continue
+
         file_path = item['path']
         if file_path.endswith('.pdf'):
             text = extract_pdf(file_path)
@@ -77,7 +87,8 @@ def extract_udhr(lang_initialcap,force=False):
             print(f"Error: Unsupported file type for {file_path}. Only pdf and txt files are supported.",file=sys.stderr)
             exit(1)
 
-        tokens = preprocess_text(text)
+        words = preprocess_text_into_words(text)
+        tokens = filter_words(words,min_char_len=3)
         common_words = get_common_words(tokens)
 
         if (verbose>1):
@@ -86,23 +97,30 @@ def extract_udhr(lang_initialcap,force=False):
         # Check if file exists and act according to `force` parameter
         if not os.path.exists(filename) or force:
             utils.save_to_json(dict(common_words), filename)
-
-def get_lang_paragraphs(queries,lang_uc):
-
-    lang_paras = []
-    
-    for query in queries:
-        url_id       = query[0]
-        url_filehash = query[5]
-        url_doctype  = query[6]
-        url_fulllang = query[9]
-
-        if (url_fullang == lang_uc):
-            # Read it in
-            # apply NLP per paragraph
-            x =5
+        else:
+            print(f"  Output file '{filename}' already exists.")
+            print(f"  To regenerate/update this file, remove this file first before running the script, or use -force to overwrite.")
             
-    return lang_paras
+def get_lang_paragraphs(urls,lang_uc):
+
+    lang_all_paras = []
+
+    downloads_dir = globals.config['downloads_dir']
+    
+    for url in urls:
+        print(f"url_row = {url}")
+        url_filehash = url[5]
+        url_doctype  = url[6]
+        nlp_para_count_lrl = url[11]
+        
+        if (nlp_para_count_lrl>0):
+            filepath,rejected_filepath = utils.get_download_filename_pair(downloads_dir,url_filehash,url_doctype)
+            text =nlp.extract_text_from_file(filepath, url_doctype)
+                
+            num_para_chunks_unused,lrl_lang_match_count_unused,lrl_paras = nlp.detect_para_language_lingua(text,lang_uc)
+            lang_all_paras.extend(lrl_paras)
+            
+    return lang_all_paras
 
 def extract_dict(lang_initialcap,force=False):
 
@@ -111,11 +129,13 @@ def extract_dict(lang_initialcap,force=False):
     from that generate the word-based frequency counts to save as JSON"""
 
     lang_uc = lang_initialcap.upper()
-    
     verbose = globals.verbose
+
+    database_filename = globals.config.get('database_file')
+    sql.set_db_filename(database_filename)
     
     config_languages = globals.config['languages']
-
+    
     for config_language, item in config_languages.items():
         # print(f"args.lang={lang_initialcap}, config_language = {config_language}")
         
@@ -126,22 +146,27 @@ def extract_dict(lang_initialcap,force=False):
 
         # Check if file exists and act according to `force` parameter
         filename = f"dicts/common_words_{config_language.lower()}.json"
-        if os.path.exists(filename):
-            print(f"  Skipping generating output file '{filename}' as this already exists.")
-            print(f"  To regenerate/update this file, remove this file before running the script, or use -force to overwrite.")
-            continue
+#        if os.path.exists(filename):
+#            print(f"  Skipping generating output file '{filename}' as this already exists.")
+#            print(f"  To regenerate/update this file, remove this file before running the script, or use -force to overwrite.")
+#            continue
 
-        queries = sql.get_all_queries(lang_uc, handled=True)
-        lang_paras = get_lang_paragraphs(queries,lang_uc)
-            
-        common_words = []
+        urls = sql.get_all_urls_filter_downloaded_handled(downloaded=True, handled=True)
+        lang_all_paras = get_lang_paragraphs(urls,lang_uc)
+
+        text = "\n".join(lang_all_paras)
+        words = preprocess_text_into_words(text)
+        tokens = filter_words(words,min_char_len=3)        
+        common_words = get_common_words(tokens)
         
-
         if (verbose>1):
             print(json.dump(dict(common_words), fp=sys.stdout, ensure_ascii=False, indent=4))
             
         # Check if file exists and act according to `force` parameter
         if not os.path.exists(filename) or force:
             utils.save_to_json(dict(common_words), filename)
-    
+        else:
+            print(f"  Output file '{filename}' already exists.")
+            print(f"  To regenerate/update this file, remove this file first before running the script, or use -force to overwrite.")
+            
     return
